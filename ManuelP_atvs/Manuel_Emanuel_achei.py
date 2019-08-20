58f371b54992f75599351eed616df3efcc04e910
XPerezX <xthepro69@gmail.com>
2019-08-20 19:06:00 -0300

    Atividade de achar o 3 atualizada

--- ManuelP_atvs/Manuel_Emanuel_achei.py
+++ ManuelP_atvs/Manuel_Emanuel_achei.py
@@ -8,7 +8,11 @@ for i in range(100):
 for k in nums:
 
     if (nums[k]%3 == 0  ):
-        nums[k]="achei"
+        nums[k]="multiplo"
+    r="3" in str(k)
+    if r :
+        nums[k]="tem 3"
 
     print (nums[k])
+
 
